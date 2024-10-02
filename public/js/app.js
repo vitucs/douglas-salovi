c474f1aa25cd2ef333835a446cba4029a1bdf7e9
Victor Coutinho <victorkclic@hotmail.com>
2024-10-02 07:22:05 -0300

    vite

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/css/app.css":
+/*!*******************************!*\
+  !*** ./resources/css/app.css ***!
+  \*******************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/dist/cjs.js):\nC:\\Users\\SAMSUNG\\douglas-salovi\\postcss.config.js:1\nexport default {\n^^^^^^\n\nSyntaxError: Unexpected token 'export'\n    at wrapSafe (node:internal/modules/cjs/loader:1378:20)\n    at Module._compile (node:internal/modules/cjs/loader:1428:41)\n    at Module._extensions..js (node:internal/modules/cjs/loader:1548:10)\n    at Module.load (node:internal/modules/cjs/loader:1288:32)\n    at Module._load (node:internal/modules/cjs/loader:1104:12)\n    at Module.require (node:internal/modules/cjs/loader:1311:19)\n    at module.exports (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\import-fresh\\index.js:32:59)\n    at loadJs (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\loaders.js:16:18)\n    at Explorer.loadFileContent (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\Explorer.js:84:32)\n    at Explorer.createCosmiconfigResult (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\Explorer.js:89:36)\n    at Explorer.loadSearchPlace (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\Explorer.js:70:31)\n    at async Explorer.searchDirectory (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\Explorer.js:55:27)\n    at async run (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\Explorer.js:35:22)\n    at async cacheWrapper (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\cacheWrapper.js:16:18)\n    at async cacheWrapper (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\cacheWrapper.js:16:18)\n    at async cacheWrapper (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\cacheWrapper.js:16:18)\n    at async Explorer.search (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\cosmiconfig\\dist\\Explorer.js:27:20)\n    at async loadConfig (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\postcss-loader\\dist\\utils.js:68:16)\n    at async Object.loader (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\postcss-loader\\dist\\index.js:54:22)\n    at processResult (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\webpack\\lib\\NormalModule.js:889:19)\n    at C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\webpack\\lib\\NormalModule.js:1030:5\n    at C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\loader-runner\\lib\\LoaderRunner.js:400:11\n    at C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\loader-runner\\lib\\LoaderRunner.js:252:18\n    at context.callback (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at Object.loader (C:\\Users\\SAMSUNG\\douglas-salovi\\node_modules\\postcss-loader\\dist\\index.js:56:7)");
+
+/***/ }),
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ (() => {
+
+throw new Error("Module parse failed: 'import' and 'export' may appear only with 'sourceType: module' (1:0)\nFile was processed with these loaders:\n * ./node_modules/babel-loader/lib/index.js\nYou may need an additional loader to handle the result of these loaders.\n> import './bootstrap';\n| import Alpine from 'alpinejs';\n| window.Alpine = Alpine;");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	__webpack_modules__["./resources/js/app.js"]();
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/css/app.css"]();
+/******/ 	
+/******/ })()
+;
